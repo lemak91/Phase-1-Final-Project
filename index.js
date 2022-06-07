fc0727f498bb5e424ec1c482a8373ead918f8e08
Jay <lemak91@mac.com>
2022-06-07 11:36:58 -0500

    add js code

--- index.js
+++ index.js
@@ -3,55 +3,45 @@ fetch("https://api.punkapi.com/v2/beers")
   .then((beersArray) => {
     console.log(beersArray);
     beersArray.forEach(addBeer);
-  });
 
-function addBeer(beerObj) {
-  // console.log(filmObj)
-  let listItem = document.createElement("li");
-  listItem.textContent = beerObj.name;
-  listItem.id = beerObj.id;
-  let beersContainer = document.querySelector("ul");
-  beersContainer.append(listItem);
-  // console.log(listItem)
-}
+    setData(beersArray[0]);
 
-button.addEventListener("click", () => {
-  const ticksRemaining = document.getElementById("ticket-num").textContent;
-  const newTicksRemaining = ticksRemaining - 1;
-  document.getElementById("ticket-num").textContent = newTicksRemaining;
+    const button = document.getElementById("buy-ticket");
 
-  button.disabled = newTicksRemaining === 0;
-});
+    button.addEventListener("click", () => {
+      const ticksRemaining = document.getElementById("ticket-num").textContent;
+      const newTicksRemaining = ticksRemaining - 1;
+      document.getElementById("ticket-num").textContent = newTicksRemaining;
 
-   let beerContainer = document.querySelector("ul");
+      button.disabled = newTicksRemaining === 0;
+    });
+
     beerContainer.addEventListener("click", (event) => {
       const id = event.target.id;
 
       if (id) {
-        setData(beerArray[id - 1]);
+        setData(beersArray[id - 1]);
         button.disabled = false;
       }
     });
+  });
 
-// function addBeer(beerObj) {
-//   // console.log(beerObj)
-//   let listItem = document.createElement("li");
-//   listItem.textContent = beerObj.title;
-//   listItem.id = beerObj.id;
-//   let beerContainer = document.querySelector("ul");
-//   beerContainer.append(listItem);
-//   // console.log(listItem)
-// }
+function addBeer(beerObj) {
+  // console.log(beerObj)
+  let listItem = document.createElement("li");
+  listItem.textContent = beerObj.name;
+  listItem.id = beerObj.id;
+  let beerContainer = document.querySelector("ul");
+  beerContainer.append(listItem);
+  // console.log(listItem)
+}
+
 
 function setData(element) {
   console.log(element);
   document.getElementById("image_url").src = element.image_url;
   document.getElementById("name").textContent = element.name;
-  document.getElementById("beer-info").textContent = element.beer-info;
+  document.getElementById("beer-info").textContent = element.beer - info;
   document.getElementById("description").textContent = element.description;
+  document.getElementById("tagline").textContent = element.tagline;
 }
-
-
-
-
-
