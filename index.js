633ac1746f6869ac7cb7429a36d6f2cf66b9eb69
Jay <lemak91@mac.com>
2022-06-08 13:19:41 -0500

    clean up format js

--- index.js
+++ index.js
@@ -3,19 +3,8 @@ fetch("https://api.punkapi.com/v2/beers")
   .then((beersArray) => {
     console.log(beersArray);
     beersArray.forEach(addBeer);
-   
 
     setData(beersArray[0]);
-
-    // const button = document.getElementById("buy-ticket");
-
-    // button.addEventListener("click", () => {
-    //   const ticksRemaining = document.getElementById("ticket-num").textContent;
-    //   const newTicksRemaining = ticksRemaining - 1;
-    //   document.getElementById("ticket-num").textContent = newTicksRemaining;
-
-    //   button.disabled = newTicksRemaining === 0;
-    // });
 
     let beerContainer = document.querySelector("ul");
     beerContainer.addEventListener("click", (event) => {
@@ -29,7 +18,7 @@ fetch("https://api.punkapi.com/v2/beers")
 function addBeer(beerObj) {
   // console.log(beerObj)
   let listItem = document.createElement("li");
-  listItem.textContent = beerObj.name
+  listItem.textContent = beerObj.name;
   listItem.id = beerObj.id;
   let beerContainer = document.querySelector("ul");
   beerContainer.append(listItem);
@@ -37,45 +26,40 @@ function addBeer(beerObj) {
 }
 
 function fetchPage() {
-    fetch(`https://api.punkapi.com/v2/beers?page=${pageNumber}`)
-      .then((response) => response.json())
-      .then((beersArray) => {
-        let beerContainer = document.querySelector("ul");
-        beerContainer.replaceChildren();
-        beersArray.forEach(addBeer);
-      });
+  fetch(`https://api.punkapi.com/v2/beers?page=${pageNumber}`)
+    .then((response) => response.json())
+    .then((beersArray) => {
+      let beerContainer = document.querySelector("ul");
+      beerContainer.replaceChildren();
+      beersArray.forEach(addBeer);
+    });
 }
 
 let pageNumber = 1;
-let nextButton = document.getElementById("next")
+let nextButton = document.getElementById("next");
 let backButton = document.getElementById("back");
-
 
 nextButton.addEventListener("click", (event) => {
   pageNumber++;
   fetchPage();
-  backButton.classList.remove("disabled")
+  backButton.classList.remove("disabled");
   if (pageNumber === 13) {
-    nextButton.classList.add("disabled")
+    nextButton.classList.add("disabled");
   }
-
-})
+});
 
 backButton.addEventListener("click", (event) => {
   pageNumber--;
   fetchPage();
   nextButton.classList.remove("disabled");
   if (pageNumber === 1) {
-    backButton.classList.add("disabled")
+    backButton.classList.add("disabled");
   }
 });
-
-
 
 // fetch("https://api.punkapi.com/v2/beers?page=2&per_page=25")
 //   .then((response) => response.json())
 //   .then((beersArray) => {
-
 
 function setData(element) {
   console.log(element);
@@ -84,9 +68,10 @@ function setData(element) {
   // document.getElementById("beer-info").textContent = element.beer - info;
   document.getElementById("description").textContent = element.description;
   document.getElementById("tagline").textContent = element.tagline;
-  document.getElementById("firstbrewed").textContent = "First Brewed " + element.first_brewed;
+  document.getElementById("firstbrewed").textContent =
+    "First Brewed " + element.first_brewed;
   document.getElementById("abv").textContent = "ABV " + element.abv;
-  document.getElementById("foodpairing").textContent = "Food Pairing:  " + element.food_pairing;
+  document.getElementById("foodpairing").textContent =
+    "Food Pairing:  " + element.food_pairing;
   // document.getElementById("ingredients").textContent = "Ingredients:  " + element.ingredients;
-
 }
